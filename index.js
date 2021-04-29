f11b7f3e021f6a2d3dce62990b6a0e74732eb4eb
angelo.p <mrpalaiologos@gmail.com>
2021-04-29 15:28:28 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,38 @@
+let clicked = false;
+document.querySelector(".clicker").addEventListener("click", () => {
+  clicked = !clicked;
+  console.log(clicked);
+  if (clicked) {
+    document.querySelector(".sun").style.cssText = "display:inline;";
+    document.querySelector("body").style.cssText = "animation-name: sunset; ";
+  } else {
+    document.querySelector(".sun").style.cssText = "display: none;";
+    document.querySelector("body").style.cssText = "animation-name: none; ";
+  }
+});
+
+let year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = year + 300 + " AD";
+
+//for the bunny
+let hStandPoint = 00;
+
+function goRight() {
+  hStandPoint += 10;
+  rabbit.style.left = hStandPoint + "px";
+}
+function goLeft() {
+  hStandPoint -= 10;
+  rabbit.style.left = hStandPoint + "px";
+}
+function move(e) {
+  if (e.code == "KeyA") {
+    goRight();
+  }
+  if (e.code == "KeyD") {
+    goLeft();
+  }
+}
+const rabbit = document.querySelector(".rabbit");
+const body = document.querySelector("body");
+body.addEventListener("keypress", move);
